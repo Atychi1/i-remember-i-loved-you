7381a639d2352c9ec69bff5a86c132eecb654277
Atychi1 <platinumninja44@gmail.com>
2024-03-25 17:56:20 +0000

    trying to fix up inv system

--- Assets/InspectItemsAndObjects/Scripts/ItemSO.cs
+++ Assets/InspectItemsAndObjects/Scripts/ItemSO.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu()]
-public class ItemSO : ScriptableObject {
+public class ItemSO : ScriptableObject
+{
+
+    public string itemName; // Name of the item
+
+    [SerializeField, TextArea(3, 20)] // Specify the number of lines visible
+    public string lore; // Lore or description of the item
 
     public Sprite sprite;
     public Transform prefab;
-
 }
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 interface IInteractable
 {
-    public void Interact();
+   public void Interact();
 }
 
 public class Interaction : MonoBehaviour
